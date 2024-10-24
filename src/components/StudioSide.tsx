5e2fb046adac75013ffca833d72c4935ecabb8b6
Corey Hayden <cghayden@gmail.com>
2024-10-24 12:05:36 -0400

    active path highlight on studio sidebar

--- src/components/StudioSide.tsx
+++ src/components/StudioSide.tsx
@@ -3,7 +3,8 @@
 // import Image from 'next/image'
 import Link from 'next/link'
 // import Logo from '@/images/pardy.png'
-import { Button } from '@nextui-org/react'
+import { Button, cn } from '@nextui-org/react'
+import { usePathname } from 'next/navigation'
 
 const links = [
   { route: '/studio', name: 'Home' },
@@ -13,23 +14,34 @@ const links = [
   { route: '/studio/settings', name: 'Settings' },
 ]
 
+const isActive = (path: string, route: string) => {
+  // all routes other than auth routes include "/dashboard"
+  // so handle that first
+  if (route === '/studio') {
+    return path === '/studio'
+  } else {
+    return path.includes(route)
+  }
+}
+
 const StudioSide = () => {
-  // const activeClass = 'bg-primary hover:bg-primary'
+  const path = usePathname()
+  const activeClass = 'bg-zinc-50 hover:bg-zinc-100'
 
   return (
     <div className='w-full h-full px-3 relative bg-zinc-300'>
       <div className='flex py-8'>
         <h1 className='text-2xl font-bold'>StudioNotes</h1>
-        {/* <figure className='w-[80px] pt-4'> */}
-        {/* <Image src={Logo} alt='pardy' /> */}
-        {/* </figure> */}
       </div>
       <div>
         {links.map((link) => (
           <div className='w-full' key={link.route}>
             <Link href={link.route}>
               <div
-                className={`w-full h-full py-2 px-2 hover:bg-content1 rounded-lg `}
+                className={cn(
+                  'w-full h-full py-2 px-2 hover:bg-content1 rounded-lg',
+                  isActive(path, link.route) && activeClass
+                )}
               >
                 {link.name}
               </div>
