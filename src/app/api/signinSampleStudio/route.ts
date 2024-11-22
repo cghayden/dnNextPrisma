b4918a7e5e6f579a71d718578f5cb9351da79428
Corey Hayden <cghayden@gmail.com>
2024-11-22 10:51:56 -0500

    signin sample studio with form data

--- src/app/api/signinSampleStudio/route.ts
+++ src/app/api/signinSampleStudio/route.ts
@@ -1,5 +1,13 @@
-import signinSampleStudio from '@/actions/auth/signinSampleStudio'
+// import { NextResponse } from 'next/server'
+
+// import signinSampleStudio from '@/actions/auth/signinSampleStudio'
+import { signin } from '@/actions/auth/signin'
 
 export async function GET() {
-  await signinSampleStudio()
+  const formData = new FormData()
+  formData.append('email', 'dancedynamics@example.com')
+  formData.append('password', 'dancedynamics')
+
+  await signin(null, formData)
+  // await signinSampleStudio()
 }
