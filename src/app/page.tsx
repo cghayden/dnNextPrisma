74cddb7c0191670a7c3e15f107ce24429b47e509
Corey Hayden <cghayden@gmail.com>
2024-11-22 11:58:24 -0500

    update cookie secrets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,11 +42,9 @@ export default function Welcome() {
           </div>
           <div className='relative text-gray-50'>
             <div className='flex flex-col md:flex-row flex-wrap'>
-              {/* <SigninSampleStudioForm /> */}
+              <SigninSampleStudioForm />
               <br />
-              {/* <a className='whitespace-nowrap' href='loginSampleParent'>
-                Login To Sample Parent Account
-              </a> */}
+              {/* <SigninSampleParentForm /> */}
             </div>
           </div>
         </div>
